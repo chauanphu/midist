4521a4e619a4fe3cd7c8f9e36a8b8d25dde9274b
nhan0204 <phamthiennhan0204@gmail.com>
2024-09-29 15:26:51 +0700

    midis-roue-fix

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -2,25 +2,27 @@ import { motion } from 'framer-motion';
 import React from 'react';
 
 const Hero = ({ header, description, prompt, background }) => {
-  const backgroundStyle = background ? {
-    backgroundImage: `url(${background})`,
-    backgroundSize: 'cover',
-    backgroundRepeat: 'no-repeat',
-    backgroundPosition: 'center',
-    height: "100vh",
-    zIndex: 1,
-  } : {};
-
+  const backgroundStyle = background
+    ? {
+        backgroundImage: `url(${background})`,
+        backgroundSize: 'cover',
+        backgroundRepeat: 'no-repeat',
+        backgroundPosition: 'center',
+        height: '100vh',
+        zIndex: 1
+      }
+    : {};
 
   return (
     <>
       {/* Overlayer */}
-      {background && <div className="absolute h-[110%] md:h-[106%] lg:h-[111%] xl:h-[107%] inset-0 bg-black opacity-30 z-2"></div>}
+      {background && (
+        <div className="absolute h-[110%] md:h-[106%] lg:h-[111%] xl:h-[107%] inset-0 bg-black opacity-30 z-2"></div>
+      )}
       <div
         style={backgroundStyle}
-        className="hero bg-gray-900 text-white h-screen flex items-center justify-center text-center">
-
-
+        className="hero bg-gray-900 text-white h-screen flex items-center justify-center text-center"
+      >
         <motion.div
           initial={{ opacity: 0, y: -50 }}
           animate={{ opacity: 1, y: 0 }}
@@ -28,14 +30,9 @@ const Hero = ({ header, description, prompt, background }) => {
           className="max-w-4xl z-2"
         >
           {/* Overlay to darken the background */}
-          <h1 className="text-5xl font-bold mb-4 drop-shadow-xl">
-            {header}
-          </h1>
-          <p className="text-xl mb-6 drop-shadow-xl">
-            {description}
-          </p>
+          <h1 className="text-5xl font-bold mb-4 drop-shadow-xl">{header}</h1>
+          <p className="text-xl mb-6 drop-shadow-xl">{description}</p>
           <motion.a
-            href="#projects"
             whileHover={{ scale: 1.1 }}
             whileTap={{ scale: 0.9 }}
             className="inline-block px-6 py-3 bg-indigo-600 rounded-lg text-lg font-medium"
@@ -45,7 +42,7 @@ const Hero = ({ header, description, prompt, background }) => {
         </motion.div>
       </div>
     </>
-  )
-}
+  );
+};
 
 export default Hero;
